f7387df4b4c2916bdf310c42b6eb443d3217b5ec
Diegoisland <mikolaj.rzepielarp@gmail.com>
2026-01-24 19:24:44 +0100

    edited author in footer, "payment" site and js
    edited author, "payment": buttons edited and more, site, peselCheck()

--- deal/deal.js
+++ deal/deal.js
@@ -1,45 +1,56 @@
-function weryfikacja()
+function peselCheck(pesel)
 {
-	let poprawnosc = false;
-
-	let pesel = document.getElementById("pesel").value;
-
 	if( ( pesel < 10000000000 ) || ( pesel > 99999999999 ) )
-		document.getElementById('error').style.visibility = "visible";
+		return false;
 	else 
 	{
 		let tab = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];	
-		//console.log(pesel[1]);
 	
 		let suma = 0, m, r = 0;
     	for (let i = 0; i < 10; i++){
         	suma += pesel[i] * tab[i];
 		}
 
-		//console.log(suma);
    		m = suma % 10;
     	if (m > 0)
         	r = 10 - m;
 
-    	if (r == pesel[10])// poprawnosc = true;
-		{
-			document.getElementById('sukces').style.visibility = "visible";
-			setTimeout("lag()", 1000);
-		}
-		else 
-			document.getElementById('error').style.visibility = "visible";
+		if (r == pesel[10])
+			return true;
+		else
+			return false;
 	}
-
-
-    return poprawnosc;
 }
-function lag()
+
+function weryfikacjaZgloszenia()
 {
-	//while(true) 
-		console.log(1);	
+	let pesel = document.getElementById("pesel").value;
+
+	let poprawnosc = peselCheck(pesel);
+	
+	if(poprawnosc)
+		document.getElementById('sukces').style.visibility = "visible";
+	else 
+		document.getElementById('error').style.visibility = "visible";
 }
+
+function fix()
+{
+	document.getElementById("shaker").classList.add("shake");
+	document.getElementById("hm").classList.add("I_forgot", "noticeMe");
+
+	//document.getElementById("I_forgot").classList.add("noticeMe");
+
+
+
+	document.getElementById("troll").style.display = 'block';
+	document.getElementById("sender_button").style.display = 'none';
+	document.getElementById("sender_submit").style.display = 'inline-block';
+	
+	document.getElementById("troll").classList.add("visibility_anim");
+	document.getElementById("hm").style.display = 'block';}
+
 function tak()
 {
 	document.getElementById('error').style.visibility = "hidden";
-	
 }
